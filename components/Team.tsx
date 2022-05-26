5f05e15e1a820667c70694f9321db61948f2dfd8
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-05-26 10:47:36 +0430

    complete team section

--- components/Team.tsx
+++ components/Team.tsx
@@ -1,4 +1,4 @@
-import React, { FC , useState} from 'react'
+import React, { FC, useState } from 'react'
 
 const Team: FC = () => {
     const team = [{ "name": 'Shabi', "role": 'Backend Developer', "image": '/images/sample_team_image.png' },
@@ -9,18 +9,18 @@ const Team: FC = () => {
 
     const nextSlide = () => {
         let newSlide =
-          currentSlide === team.length - 1
-            ? 0
-            : currentSlide + 1;
-            setCurrentSlide(newSlide);
-      };
-      const prevSlide = () => {
+            currentSlide === team.length - 1
+                ? 0
+                : currentSlide + 1;
+        setCurrentSlide(newSlide);
+    };
+    const prevSlide = () => {
         let newSlide =
-          currentSlide === 0
-            ? team.length - 1
-            : currentSlide - 1;
-            setCurrentSlide(newSlide);
-      };
+            currentSlide === 0
+                ? team.length - 1
+                : currentSlide - 1;
+        setCurrentSlide(newSlide);
+    };
     return (
 
         <div className='mt-[151px] flex flex-col items-center mb-40'>
@@ -28,26 +28,33 @@ const Team: FC = () => {
                 <p className='text-gradient1'>Our Professionals</p>
             </div>
             <p className='text-[45px] font-medium text-white mt-[10px]'>Our Team</p>
-            <div className='mt-16'>
+            <div className='mt-16 flex items-center'>
+                <section className='bg-[#2b2b42] flex items-center justify-center rounded-full h-[56px] w-[56px] mr-[40px] hover:cursor-pointer'>
+                    <img src='/images/leftArrowWhite.svg' onClick={nextSlide} alt='arrow' />
+                </section>
+
+                {team[currentSlide + 1]?.image && <img src={team[currentSlide + 1]?.image} className="w-[10.5rem] h-[12.125rem] mr-[24px]" />}
                 {team.map((item, index) =>
-                    <section key={index}  className={
+                    <section key={index} className={
                         index === currentSlide
-                          ? "object-cover w-[46.5rem] h-[18.1875rem] bg-[#121424] rounded-[10px] flex items-center"
-                          : "hidden"
-                      }>
+                            ? "object-cover w-[46.5rem] h-[18.1875rem] bg-[#121424] rounded-[10px] flex items-center"
+                            : "hidden"
+                    }>
                         <img src={item.image} alt={item.name} loading='lazy' className='w-[18.625rem] h-[15rem] ml-[1.56rem] object-cover rounded-[5px]' />
                         <section className='ml-12'>
                             <p className='text-white text-4xl'>{item.name}</p>
                             <p className='text-[#7981A3] text-[22px]'>{item.role}</p>
                             <div className='flex mt-8 justify-between'>
-                                <img src='/images/facebook.svg' alt='facebook' loading='lazy'/>
-                                <img src='/images/facebook.svg' alt='facebook' loading='lazy'/>
-                                <img src='/images/facebook.svg' alt='facebook' loading='lazy'/>
+                                <img src='/images/facebook.svg' alt='facebook' loading='lazy' />
+                                <img src='/images/facebook.svg' alt='facebook' loading='lazy' />
+                                <img src='/images/facebook.svg' alt='facebook' loading='lazy' />
                             </div>
                         </section>
                     </section>)}
-                    <p className='text-white' onClick={nextSlide}>next</p>
-                    <p className='text-white' onClick={prevSlide}>prev</p>
+
+                <section className='bg-[#2b2b42] flex items-center justify-center rounded-full h-[56px] w-[56px] ml-[40px] hover:cursor-pointer'>
+                    <img src='/images/rightArrowWhite.svg' onClick={prevSlide} alt='arrow'/>
+                </section>
             </div>
 
         </div>
