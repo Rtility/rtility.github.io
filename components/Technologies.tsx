9386c39ae5cb45d168bcd76a4af90ed2777891a1
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-05-28 15:05:36 +0430

    responsive technologies

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Lottie from 'react-lottie';
 import animationData from './lotties/techAnimation.json'
+import mobileAnimationData from './lotties/mobileTechAnimation.json'
 import { useInView } from "react-intersection-observer";
 
 const Technologies = () => {
   const { ref, inView } = useInView();
+  const [clientWidth, setClientWidth] = useState<number>(0);
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -13,16 +16,30 @@ const Technologies = () => {
       preserveAspectRatio: 'xMidYMid slice'
     }
   };
+  const mobileDefaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: mobileAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setClientWidth(window.innerWidth)
+    }
+  }, [])
+
   return (
     <div className='mt-[11.875rem]'>
-      <p className='text-[2.8125rem] text-white font-medium text-center'>Our Technologies</p>
-      <div ref={ref}>
-        {inView && <Lottie options={defaultOptions} style={{cursor:'default'}}/>}
+      <p className='text-[1.75rem] sm:text-[2.8125rem] text-white font-medium text-center'>Our Technologies</p>
+      <div ref={ref} className='mt-8 sm:mt-0'>
+        {inView && clientWidth <= 640 ? <Lottie options={mobileDefaultOptions} style={{ cursor: 'default', width: '95%' }} /> : <Lottie options={defaultOptions} style={{ cursor: 'default', width: '86%' }} />}
       </div>
-      <section className='text-[#7981A3] font-normal text-[28px] flex justify-around'>
+      <section className='hidden sm:flex text-[#7981A3] font-normal md:text-[20px] lg:text-[28px] justify-around'>
         <p>Design</p>
         <p>Develop</p>
-        <p className='relative right-12'>Test</p>
+        <p>Test</p>
       </section>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
       <main className='container mx-auto'>
         <Partners />
         <Services />
-       {/* <Technologies /> */}
+       <Technologies />
        {/*  <Team /> */}
       </main>
       <footer>
