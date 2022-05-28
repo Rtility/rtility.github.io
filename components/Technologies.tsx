8683f143c8d05af61189de2c6bf7d24a45a915a2
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-05-28 11:56:14 +0430

    show technologies animation when in view port

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Lottie from 'react-lottie';
 import animationData from './lotties/techAnimation.json'
+import { useInView } from "react-intersection-observer";
 
 const Technologies = () => {
+  const { ref, inView } = useInView();
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -14,7 +16,9 @@ const Technologies = () => {
   return (
     <div className='mt-[11.875rem]'>
       <p className='text-[2.8125rem] text-white font-medium text-center'>Our Technologies</p>
-      <Lottie options={defaultOptions} height={550} width={1200} style={{cursor:'default'}}/>
+      <div ref={ref}>
+        {inView && <Lottie options={defaultOptions} height={550} width={1200} style={{cursor:'default'}}/>}
+      </div>
       <section className='text-[#7981A3] font-normal text-[28px] flex justify-around'>
         <p>Design</p>
         <p>Develop</p>
