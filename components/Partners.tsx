027b198470b26b6fafd813f3023033600cbd8a59
Amirhossein Banavi <amirhossein.banavi@gmail.com>
2022-06-01 07:22:59 +0000

    add construction message in partners components

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -20,6 +20,7 @@ const Partners: FC = () => {
     };
     return (
         <div className='text-white mt-[9.375rem] md:mt-[7.25rem] flex flex-col items-center'>
+            <p className='text-center lg:text-left text-[36px] lg:text-[57px] font-semibold mt-[1.69rem]'>🚧 <span className='text-gradient1'> This Website Is Under Construction</span> 🚧</p>
             <section className='block lg:hidden bg-[#00D2EF] w-[300px] h-[300px] rounded-full opacity-20 blur-[150px] absolute right-0 -mt-18 -z-10' />
             <div className='bg-[#131938] w-[4.25rem] h-[2.25rem] rounded-[5px] text-xs font-normal flex items-center justify-center'>
                 <p className='text-gradient1'>We Love</p>
